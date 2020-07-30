b41c6e6a7447a91f36256c29580263d0d72bfaa5
kaida <cakeandkta@gmail.com>
2020-07-31 03:26:49 +0900

    first commit

---
+++ login/__init__.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '064a6d1cd7458a0b6e9c83263b679265'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sample.sqlite3'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from login import routes
